56dcda68cfd11857f4c782af9e67d4dd07a992ee
ryanbow <ryanbowz@outlook.com>
2022-09-10 20:53:25 +0800

    Site updated: 2022-09-10 20:53:21

---
+++ preview/1/Common/README.txt
@@ -0,0 +1,8 @@
+Common files:
+
+webgl-utils.js: standard utilities from google to set up a webgl context
+
+MV.js: our matrix/vector package. Documentation on website，加了注解并修改了部分
+
+initShaders.js: functions to initialize shaders in the html file
+
---
+++ preview/1/Common/initShaders.js
@@ -0,0 +1,59 @@
+//
+//  initShaders.js
+//
+
+function initShaders( gl, vertexShaderId, fragmentShaderId )
+{
+    var vertShdr;
+    var fragShdr;
+	//从html文件中读取顶点着色器文档ID，创建顶点着色器对象，编译着色器
+    var vertElem = document.getElementById( vertexShaderId );
+    if ( !vertElem ) { 
+        alert( "Unable to load vertex shader " + vertexShaderId );
+        return -1;
+    }
+    else {
+        vertShdr = gl.createShader( gl.VERTEX_SHADER );
+        gl.shaderSource( vertShdr, vertElem.text );
+        gl.compileShader( vertShdr );
+        if ( !gl.getShaderParameter(vertShdr, gl.COMPILE_STATUS) ) {
+            var msg = "Vertex shader failed to compile.  The error log is:"
+        	+ "<pre>" + gl.getShaderInfoLog( vertShdr ) + "</pre>";
+            alert( msg );
+            return -1;
+        }
+    }
+
+	//从html文件中读取片元着色器文档ID，创建片元着色器对象，编译着色器
+    var fragElem = document.getElementById( fragmentShaderId );
+    if ( !fragElem ) { 
+        alert( "Unable to load vertex shader " + fragmentShaderId );
+        return -1;
+    }
+    else {
+        fragShdr = gl.createShader( gl.FRAGMENT_SHADER );
+        gl.shaderSource( fragShdr, fragElem.text );
+        gl.compileShader( fragShdr );
+        if ( !gl.getShaderParameter(fragShdr, gl.COMPILE_STATUS) ) {
+            var msg = "Fragment shader failed to compile.  The error log is:"
+        	+ "<pre>" + gl.getShaderInfoLog( fragShdr ) + "</pre>";
+            alert( msg );
+            return -1;
+        }
+    }
+   
+    //创建 program对象，将编译通过的着色器对象链接到程序对象中进行保存。
+    var program = gl.createProgram();
+    gl.attachShader( program, vertShdr );
+    gl.attachShader( program, fragShdr );
+    gl.linkProgram( program );
+    
+    if ( !gl.getProgramParameter(program, gl.LINK_STATUS) ) {
+        var msg = "Shader program failed to link.  The error log is:"
+            + "<pre>" + gl.getProgramInfoLog( program ) + "</pre>";
+        alert( msg );
+        return -1;
+    }
+
+    return program;
+}
